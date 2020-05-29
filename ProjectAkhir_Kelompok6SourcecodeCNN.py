f28e116612271d8b8e453c2fca5c6a78cb2e8536
rampfox <36215916+rampfox@users.noreply.github.com>
2020-05-29 15:51:29 +0700

    Add files via upload

---
+++ ProjectAkhir_Kelompok6SourcecodeCNN.py
@@ -0,0 +1,272 @@
+import tensorflow as tf
+
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers import Dense, Activation, Dropout, Flatten
+
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+from time import time
+#from tensorflow.python.keras.callbacks import TensorBoard
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#cpu - gpu configuration
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 56} ) #max: 1 gpu, 56 cpu
+sess = tf.Session(config=config) 
+keras.backend.set_session(sess)
+
+#variables
+num_classes = 7 #angry, disgust, fear, happy, sad, surprise, neutral
+batch_size = 256
+epochs = 5
+
+#read kaggle facial expression recognition challenge dataset (fer2013.csv)
+#https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge
+
+with open("fer2013.csv") as f:
+    content = f.readlines()
+
+lines = np.array(content)
+
+num_of_instances = lines.size
+print("number of instances: ",num_of_instances)
+print("instance length: ",len(lines[1].split(",")[1].split(" ")))
+
+
+#initialize trainset and test set
+x_train, y_train, x_test, y_test = [], [], [], []
+
+
+#transfer train and test set data
+for i in range(1,num_of_instances):
+    try:
+        emotion, img, usage = lines[i].split(",")
+          
+        val = img.split(" ")
+            
+        pixels = np.array(val, 'float32')
+        
+        emotion = keras.utils.to_categorical(emotion, num_classes)
+    
+        if 'Training' in usage:
+            y_train.append(emotion)
+            x_train.append(pixels)
+        elif 'PublicTest' in usage:
+            y_test.append(emotion)
+            x_test.append(pixels)
+    except:
+        print("",end="")
+
+
+#data transformation for train and test sets
+x_train = np.array(x_train, 'float32')
+y_train = np.array(y_train, 'float32')
+x_test = np.array(x_test, 'float32')
+y_test = np.array(y_test, 'float32')
+
+x_train /= 255 #normalize inputs between [0, 1]
+x_test /= 255
+
+x_train = x_train.reshape(x_train.shape[0], 48, 48, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.reshape(x_test.shape[0], 48, 48, 1)
+x_test = x_test.astype('float32')
+
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+#construct CNN structure
+model = Sequential()
+
+#1st convolution layer
+model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
+
+#2nd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+#3rd convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+model.add(Flatten())
+
+#fully connected neural networks
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(num_classes, activation='softmax'))
+
+#batch process
+gen = ImageDataGenerator()
+train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
+
+#tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+model.compile(loss='categorical_crossentropy'
+    , optimizer=keras.optimizers.Adam()
+    , metrics=['accuracy']
+)
+
+
+
+fit = True
+
+if fit == True:
+	#model.fit_generator(x_train, y_train, epochs=epochs) #train for all trainset
+	model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=epochs) #train for randomly selected one
+else:
+	model.load_weights('/data/facial_expression_model_weights.h5') #load weights
+	
+
+
+#overall evaluation
+score = model.evaluate(x_test, y_test)
+print('Test loss:', score[0])
+print('Test accuracy:', 100*score[1])
+
+
+#function for drawing bar chart for emotion preditions
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    
+    plt.bar(y_pos, emotions, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('percentage')
+    plt.title('emotion')
+    
+    plt.show()
+
+
+monitor_testset_results = False
+
+if monitor_testset_results == True:
+	#make predictions for test set
+	predictions = model.predict(x_test)
+
+	index = 0
+	for i in predictions:
+		if index < 30 and index >= 20:
+			#print(i) #predicted scores
+			#print(y_test[index]) #actual scores
+			
+			testing_img = np.array(x_test[index], 'float32')
+			testing_img = testing_img.reshape([48, 48]);
+			
+			plt.gray()
+			plt.imshow(testing_img)
+			plt.show()
+			
+			print(i)
+			
+			emotion_analysis(i)
+			print("----------------------------------------------")
+		index = index + 1
+
+
+### Active code if you want predict use image ###
+### Make prediction for custom image out of test set ###
+		
+'''
+img = image.load_img("mahathir.jpg", grayscale=True, target_size=(48, 48))
+
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis = 0)
+
+x /= 255
+
+custom = model.predict(x)
+emotion_analysis(custom[0])
+
+x = np.array(x, 'float32')
+x = x.reshape([48, 48]);
+
+plt.gray()
+plt.imshow(x)
+plt.show()
+'''
+
+
+### Active code if you want predict use Webcam  ###
+### Facial Expression on streamed data code bellow ###
+
+'''
+import numpy as np
+import cv2
+from keras.preprocessing import image
+
+
+#opencv initialization
+
+face_cascade = cv2.CascadeClassifier('/home/mercurial//Project/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+#face expression recognizer initialization
+from keras.models import model_from_json
+model = model_from_json(open("facial_expression_model_structure.json", "r").read())
+model.load_weights('facial_expression_model_weights.h5') #load weights
+
+#-----------------------------
+
+emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+
+
+
+while(True):
+	ret, img = cap.read()
+	#img = cv2.imread('.jpg')
+
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	#print(faces) #locations of detected faces
+
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+		
+		detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+		detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
+		detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
+		
+		img_pixels = image.img_to_array(detected_face)
+		img_pixels = np.expand_dims(img_pixels, axis = 0)
+		
+		img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+		
+		predictions = model.predict(img_pixels) #store probabilities of 7 expressions
+		
+		#find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
+		max_index = np.argmax(predictions[0])
+		
+		emotion = emotions[max_index]
+		
+		#write emotion text above rectangle
+		cv2.putText(img, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+		
+		#process on detected face end
+		
+
+	cv2.imshow('img',img)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
+		break
+
+#kill open cv things		
+cap.release()
+cv2.destroyAllWindows()
+
+'''
